100e23e5daa10d34fe84a915245093d62e60fcfe
seveibar <seveibar@gmail.com>
2024-11-02 22:01:52 -0700

    handle HEAD request (used to probe if the server is running)

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,9 +3,13 @@ import { z } from "zod"
 
 export default withWinterSpec({
   auth: "none",
-  methods: ["GET", "POST"],
+  methods: ["HEAD", "GET", "POST"],
   jsonResponse: z.any(),
 } as const)(async (req, ctx) => {
+  if (req.method === "HEAD") {
+    return new Response(null, { status: 200 })
+  }
+
   return ctx.react(
     <div>
       <a href="/admin/categories/list">Categories</a>
