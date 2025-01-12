444c75e0f14bb48156b3051cbbc5b790765b2740
seveibar <seveibar@gmail.com>
2025-01-12 12:16:19 -0800

    add stale-while-revalidate headers

--- lib/middlewares/with-cache-headers.ts
+++ lib/middlewares/with-cache-headers.ts
@@ -5,7 +5,10 @@ export const withCacheHeaders: Middleware<{}, {}> = async (req, ctx, next) => {
 
   if (req.url.includes("tscircuit.com")) {
     // Save for 24 hours
-    res.headers.set("Cache-Control", "public, max-age=86400")
+    res.headers.set(
+      "Cache-Control",
+      "public, max-age=86400, stale-while-revalidate=86400",
+    )
     res.headers.set("Vary", "*")
   }
 
