6ba98ef4738c750b60fa3293e482a277b7de84bd
seveibar <seveibar@gmail.com>
2025-02-09 13:57:46 -0800

    format

--- lib/middlewares/with-cache-headers.ts
+++ lib/middlewares/with-cache-headers.ts
@@ -7,7 +7,7 @@ export const withCacheHeaders: Middleware<{}, {}> = async (req, ctx, next) => {
     // Cache for 1 week (604800 seconds) with 1 hour stale-while-revalidate
     res.headers.set(
       "Cache-Control",
-      "public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600"
+      "public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600",
     )
     res.headers.set("Vary", "*")
   }
