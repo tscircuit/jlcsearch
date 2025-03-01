97442706d6df5c098e816fe291be0b895389e017
ArnavK-09 <arnavkaushal09@gmail.com>
2025-03-01 16:02:39 +0000

    fix tests

--- tests/routes/api/search.test.ts
+++ tests/routes/api/search.test.ts
@@ -3,30 +3,30 @@ import { getTestServer } from "tests/fixtures/get-test-server"
 
 test("GET /components/list with search query 'C1234' returns expected components", async () => {
   const { axios } = await getTestServer()
-  const res = await axios.get("/components/list?json=true&q=C1234")
+  const res = await axios.get("/api/search?limit=1&q=C1234")
 
   expect(res.data).toHaveProperty("components")
   expect(Array.isArray(res.data.components)).toBe(true)
 
-  expect(res.data.components[0]).toMatchSnapshot()
+  expect(res.data.components).toMatchSnapshot()
 })
 
 test("GET /components/list with search query '555 Timer' returns expected components", async () => {
   const { axios } = await getTestServer()
-  const res = await axios.get("/components/list?json=true&q=555%20Timer")
+  const res = await axios.get("/api/search?limit=1&q=555%20Timer")
 
   expect(res.data).toHaveProperty("components")
   expect(Array.isArray(res.data.components)).toBe(true)
 
-  expect(res.data.components[0]).toMatchSnapshot()
+  expect(res.data.components).toMatchSnapshot()
 })
 
-test("GET /components/list with search query 'led red' returns expected components", async () => {
+test("GET /components/list with search query 'red led' returns expected components", async () => {
   const { axios } = await getTestServer()
-  const res = await axios.get("/components/list?json=true&q=led%20red")
+  const res = await axios.get("/api/search?limit=1&q=red led")
 
   expect(res.data).toHaveProperty("components")
   expect(Array.isArray(res.data.components)).toBe(true)
 
-  expect(res.data.components[0]).toMatchSnapshot()
+  expect(res.data.components).toMatchSnapshot()
 })
