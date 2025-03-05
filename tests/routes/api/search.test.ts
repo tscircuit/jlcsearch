ded0c5bf87b4ccd2120bda534bc6fb0af4e4dff7
Arnav K <arnavkaushal09@gmail.com>
2025-03-05 15:13:00 +0000

    tests: update so not to account stock

--- tests/routes/api/search.test.ts
+++ tests/routes/api/search.test.ts
@@ -8,18 +8,14 @@ test("GET /api/search with search query 'C1234' returns expected components", as
   expect(res.data).toHaveProperty("components")
   expect(Array.isArray(res.data.components)).toBe(true)
 
-  expect(res.data.components).toMatchInlineSnapshot(`
-    [
-      {
-        "description": "125mW Thin Film Resistor 100V ±25ppm/℃ ±1% 1.23MΩ 0805 Chip Resistor - Surface Mount ROHS",
-        "lcsc": 217796,
-        "mfr": "ARG05FTC1234N",
-        "package": "0805",
-        "price": 0.005642857,
-        "stock": 9,
-      },
-    ]
-  `)
+  expect(res.data.components[0]).toMatchObject({
+    description:
+      "125mW Thin Film Resistor 100V ±25ppm/℃ ±1% 1.23MΩ 0805 Chip Resistor - Surface Mount ROHS",
+    lcsc: 217796,
+    mfr: "ARG05FTC1234N",
+    package: "0805",
+    price: 0.005642857,
+  })
 })
 
 test("GET /api/search with search query '555 Timer' returns expected components", async () => {
@@ -29,18 +25,13 @@ test("GET /api/search with search query '555 Timer' returns expected components"
   expect(res.data).toHaveProperty("components")
   expect(Array.isArray(res.data.components)).toBe(true)
 
-  expect(res.data.components).toMatchInlineSnapshot(`
-    [
-      {
-        "description": "DIP-8 555 Timers / Counters ROHS",
-        "lcsc": 22461592,
-        "mfr": "LM555CN",
-        "package": "DIP-8",
-        "price": 0.141,
-        "stock": 2519,
-      },
-    ]
-  `)
+  expect(res.data.components[0]).toMatchObject({
+    description: "DIP-8 555 Timers / Counters ROHS",
+    lcsc: 22461592,
+    mfr: "LM555CN",
+    package: "DIP-8",
+    price: 0.141,
+  })
 })
 
 test("GET /api/search with search query 'red led' returns expected components", async () => {
@@ -50,23 +41,11 @@ test("GET /api/search with search query 'red led' returns expected components", 
   expect(res.data).toHaveProperty("components")
   expect(Array.isArray(res.data.components)).toBe(true)
 
-  expect(res.data.components).toMatchInlineSnapshot(`
-    [
-      {
-        "description": "-40℃~+85℃ Red 0603 LED Indication - Discrete ROHS",
-        "lcsc": 2286,
-        "mfr": "KT-0603R",
-        "package": "0603",
-        "price": 0.005314286,
-        "stock": 3208103,
-      },
-    ]
-  `)
+  expect(res.data.components[0]).toMatchObject({
+    description: "-40℃~+85℃ Red 0603 LED Indication - Discrete ROHS",
+    lcsc: 2286,
+    mfr: "KT-0603R",
+    package: "0603",
+    price: 0.005314286,
+  })
 })
-
-
-declare module "bun:test" {
-  interface Matchers<T = unknown> {
-    toMatchInlineSnapshot(snapshot?: string | null): Promise<MatcherResult>
-  }
-}
