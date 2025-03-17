119e21b6bc6ac73284258be7051ff32a27d7867b
Ayush Jhawar <ayushjhawar499@gmail.com>
2025-03-17 07:02:33 +0530

    add title

--- lib/middlewares/with-ctx-react.tsx
+++ lib/middlewares/with-ctx-react.tsx
@@ -9,16 +9,26 @@ export const withCtxReact: Middleware<
   ctx.react = (component: ReactNode) => {
     const pathComponents = new URL(req.url).pathname.split("/").filter(Boolean)
     const timezone = req.headers.get("X-Timezone") || "UTC"
+
+    // Generate title based on path
+    const pageTitle =
+      pathComponents.length > 0
+        ? pathComponents[pathComponents.length - 1].charAt(0).toUpperCase() +
+          pathComponents[pathComponents.length - 1].slice(1)
+        : "JLCPCB Parts"
+
     return new Response(
       renderToString(
         <html lang="en">
-          <meta charSet="utf-8" />
-          <script src="https://cdn.tailwindcss.com" />
-          <style
-            type="text/tailwindcss"
-            // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-            dangerouslySetInnerHTML={{
-              __html: `
+          <head>
+            <meta charSet="utf-8" />
+            <title>{pageTitle} - JLCPCB Parts Search</title>
+            <script src="https://cdn.tailwindcss.com" />
+            <style
+              type="text/tailwindcss"
+              // biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
+              dangerouslySetInnerHTML={{
+                __html: `
 a {
    @apply underline text-blue-600 hover:text-blue-800 visited:text-purple-600 m-1
 }
@@ -44,20 +54,21 @@ button {
   @apply text-center py-2 text-xs text-gray-600 border-t border-gray-300 mt-4
 }
 `,
-            }}
-          />
-          <script
-            // biome-ignore lint/security/noDangerouslySetInnerHtml: Analytics script needs to execute
-            dangerouslySetInnerHTML={{
-              __html: `
+              }}
+            />
+            <script
+              // biome-ignore lint/security/noDangerouslySetInnerHtml: Analytics script needs to execute
+              dangerouslySetInnerHTML={{
+                __html: `
 !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init capture register register_once register_for_session unregister unregister_for_session getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey getNextSurveyStep identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty createPersonProfile opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing debug getPageViewId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
     posthog.init('phc_htd8AQjSfVEsFCLQMAiUooG4Q0DKBCjqYuQglc9V3Wo', {
         api_host:'https://us.i.posthog.com',
         person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
     })
               `,
-            }}
-          />
+              }}
+            />
+          </head>
           <body>
             <div className="wrapper">
               <div className="border-b border-gray-300 py-1 flex justify-between items-center">
