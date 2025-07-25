1f90e96ca9faa5b6d5602aa6c7106834606ce415
seveibar <seveibar@gmail.com>
2025-07-25 07:56:20 -0700

    remove lockfile extend caching stale time

--- lib/middlewares/with-cache-headers.ts
+++ lib/middlewares/with-cache-headers.ts
@@ -1,16 +1,16 @@
-import type { Middleware } from "winterspec"
+import type { Middleware } from "winterspec";
 
 export const withCacheHeaders: Middleware<{}, {}> = async (req, ctx, next) => {
-  const res = await next(req, ctx)
+	const res = await next(req, ctx);
 
-  if (req.url.includes("tscircuit.com")) {
-    // Cache for 1 week (604800 seconds) with 1 hour stale-while-revalidate
-    res.headers.set(
-      "Cache-Control",
-      "public, max-age=604800, s-maxage=604800, stale-while-revalidate=3600",
-    )
-    res.headers.set("Vary", "*")
-  }
+	if (req.url.includes("tscircuit.com")) {
+		// Cache for 1 week (604800 seconds) with 24 hour stale-while-revalidate
+		res.headers.set(
+			"Cache-Control",
+			"public, max-age=604800, s-maxage=604800, stale-while-revalidate=86400",
+		);
+		res.headers.set("Vary", "*");
+	}
 
-  return res
-}
+	return res;
+};
