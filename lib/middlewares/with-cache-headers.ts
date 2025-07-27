e11ab25debb609a645d6b32f0a68b699ddace99e
Severin Ibarluzea <seve700@gmail.com>
2025-07-27 16:34:42 -0700

    Adjust caching headers (#71)
    * Adjust cache headers for 5 minute refresh
    
    * Limit 5min cache to homepage

--- lib/middlewares/with-cache-headers.ts
+++ lib/middlewares/with-cache-headers.ts
@@ -4,11 +4,22 @@ export const withCacheHeaders: Middleware<{}, {}> = async (req, ctx, next) => {
   const res = await next(req, ctx)
 
   if (req.url.includes("tscircuit.com")) {
-    // Cache for 1 week (604800 seconds) with 24 hour stale-while-revalidate
-    res.headers.set(
-      "Cache-Control",
-      "public, max-age=604800, s-maxage=604800, stale-while-revalidate=86400",
-    )
+    const path = new URL(req.url).pathname
+
+    if (path === "/") {
+      // Cache the homepage for 5 minutes, serve stale content while revalidating or on errors
+      res.headers.set(
+        "Cache-Control",
+        "public, max-age=300, s-maxage=300, stale-while-revalidate=300, stale-if-error=86400",
+      )
+    } else {
+      // Cache other pages for 1 week with 24h stale-while-revalidate
+      res.headers.set(
+        "Cache-Control",
+        "public, max-age=604800, s-maxage=604800, stale-while-revalidate=86400",
+      )
+    }
+
     res.headers.set("Vary", "*")
   }
 
