4007394216df17871ae4aa3767e88751a0222150
soongtv <283416539@qq.com>
2026-03-10 18:46:33 +0800

    fix: add is_extended_promotional to database types and resistor_array

--- lib/db/derivedtables/fpc_connector.ts
+++ lib/db/derivedtables/fpc_connector.ts
@@ -56,7 +56,7 @@ export const fpcConnectorTableSpec: DerivedTableSpec<FpcConnector> = {
           in_stock: Boolean((c.stock || 0) > 0),
           is_basic: Boolean(c.basic),
           is_preferred: Boolean(c.preferred),
-        is_extended_promotional: Boolean(c.extended_promotional),
+          is_extended_promotional: Boolean(c.extended_promotional),
           pitch_mm: parseNum(attrs["Pitch"]),
           number_of_contacts: isNaN(contacts) ? null : contacts,
           contact_type: attrs["Contact Type"] || null,
