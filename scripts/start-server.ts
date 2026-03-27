e9b29eb4811afc14e5d09f850e1fece5ced04a53
seveibar <seveibar@gmail.com>
2026-03-26 19:09:40 -0700

    fly deploy fixes

--- scripts/start-server.ts
+++ scripts/start-server.ts
@@ -2,12 +2,15 @@ import { createWinterSpecBundleFromDir } from "winterspec/adapters/node"
 import {} from "winterspec"
 import * as Path from "node:path"
 
+const hostname = process.env.HOST ?? "0.0.0.0"
+const port = Number.parseInt(process.env.PORT ?? "3065", 10)
+
 console.log(`Loading API routes from "./routes"...`)
 const winterspecBundle = await createWinterSpecBundleFromDir(
   Path.join(import.meta.dir, "../routes"),
 )
 
-console.log("Starting server on port http://localhost:3065 ...")
+console.log(`Starting server on http://${hostname}:${port} ...`)
 Bun.serve({
   fetch: (req) => {
     const response = winterspecBundle.makeRequest(req)
@@ -17,5 +20,6 @@ Bun.serve({
     [`/database/${process.env.DATABASE_DOWNLOAD_TOKEN}`]: () =>
       new Response(Bun.file("./db.sqlite3")),
   },
-  port: 3065,
+  hostname,
+  port,
 })
