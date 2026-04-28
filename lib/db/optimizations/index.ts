376d05f7417b8fc20c9e9f4e90becd6cd37cb334
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-28 19:59:53 -0400

    fix: address reviewer requested changes

--- lib/db/optimizations/index.ts
+++ lib/db/optimizations/index.ts
@@ -1,3 +1,4 @@
+// File: lib/db/optimizations/index.ts
 import { componentInStockColumn } from "./component-in-stock-column"
 import { componentBasicIndex } from "./component-basic-index"
 import { removeStaleComponents } from "./remove-stale-components"
