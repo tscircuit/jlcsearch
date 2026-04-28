678eda25418b3a0e023b16006ebefc4ebe5c589a
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-28 19:54:13 -0400

    fix: fix: add is_extended_promotional  column to components (from data source

--- routes/components/list.json.tsx
+++ routes/components/list.json.tsx
@@ -1,2 +1,16 @@
-import list from "./list"
-export default list
+import { componentExtendedPromotionalColumn } from "lib/db/optimizations/component-extended-promotional-column"
+import { componentInStockColumn } from "lib/db/optimizations/component-in-stock-column"
+import { componentBasicIndex } from "lib/db/optimizations/component-basic-index"
+import { removeStaleComponents } from "lib/db/optimizations/remove-stale-components"
+import { db } from "lib/db/db"
+import { json } from "itty-router"
+
+export const GET = async () => {
+  const components = await db
+    .selectFrom("components")
+    .selectAll()
+    .orderBy("id", "asc")
+    .execute()
+
+  return json({ components })
+}
