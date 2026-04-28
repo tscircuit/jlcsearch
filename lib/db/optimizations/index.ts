00cd13f0c929de3d975abe35e0adaa06d7579044
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-28 19:54:12 -0400

    fix: fix: add is_extended_promotional  column to components (from data source

---
+++ lib/db/optimizations/index.ts
@@ -0,0 +1,11 @@
+import { componentInStockColumn } from "./component-in-stock-column"
+import { componentBasicIndex } from "./component-basic-index"
+import { removeStaleComponents } from "./remove-stale-components"
+import { componentExtendedPromotionalColumn } from "./component-extended-promotional-column"
+
+export const dbOptimizations = [
+  componentInStockColumn,
+  componentBasicIndex,
+  removeStaleComponents,
+  componentExtendedPromotionalColumn,
+]
