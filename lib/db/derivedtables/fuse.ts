87ead28e438fa3bc6c00ef49f53b2f9bd2336436
pon024587-collab <pon024587-collab@example.com>
2026-05-17 16:17:56 +0900

    fix: correct indentation in derived tables

--- lib/db/derivedtables/fuse.ts
+++ lib/db/derivedtables/fuse.ts
@@ -129,7 +129,7 @@ export const fuseTableSpec: DerivedTableSpec<Fuse> = {
           in_stock: Boolean((c.stock || 0) > 0),
           is_basic: Boolean(c.basic),
           is_preferred: Boolean(c.preferred),
-        is_extended_promotional: Boolean(c.is_extended_promotional),
+          is_extended_promotional: Boolean(c.is_extended_promotional),
           current_rating: current_rating as number,
           voltage_rating: voltage_rating as number,
           response_time,
