bca5a8e8b8e845781defe5966904030d96c50220
1aday <git@013370.com>
2026-05-19 20:45:28 -0400

    Align extended promotional derivation

--- cf-proxy/test/is-extended-promotional.test.ts
+++ cf-proxy/test/is-extended-promotional.test.ts
@@ -4,9 +4,10 @@ import { isExtendedPromotional } from "../src/is-extended-promotional"
 describe("isExtendedPromotional", () => {
   it("derives extended promotional from preferred but not basic", () => {
     expect(isExtendedPromotional({ preferred: 1, basic: 0 })).toBe(true)
-    expect(isExtendedPromotional({ preferred: true, basic: false })).toBe(true)
     expect(isExtendedPromotional({ preferred: 1, basic: 1 })).toBe(false)
     expect(isExtendedPromotional({ preferred: 0, basic: 0 })).toBe(false)
+    expect(isExtendedPromotional({ preferred: 1, basic: null })).toBe(false)
+    expect(isExtendedPromotional({ preferred: true, basic: false })).toBe(false)
     expect(isExtendedPromotional({ preferred: null, basic: null })).toBe(false)
   })
 })
