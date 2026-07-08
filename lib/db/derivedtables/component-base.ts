b8a71c1092e492c31b73ee6de3cebc77176f1042
thienanwspace <daothienanwspace@gmail.com>
2026-07-08 12:24:02 +0700

    Refactor BaseComponent interface formatting
    Removed line breaks for the BaseComponent interface properties and added is_extended_promotional property.

--- lib/db/derivedtables/component-base.ts
+++ lib/db/derivedtables/component-base.ts
@@ -1,11 +1,2 @@
-export interface BaseComponent {
-  lcsc: number
-  mfr: string
-  description: string
-  stock: number
-  price1: number | null
-  in_stock: boolean
-  is_basic: boolean
-  is_preferred: boolean
-  attributes: Record<string, string>
-}
+export interface BaseComponent {  lcsc: number  mfr: string  description: string  stock: number  price1: number | null  in_stock: boolean
+  is_extended_promotional: boolean  is_basic: boolean  is_preferred: boolean  attributes: Record<string, string>}
